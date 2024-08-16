e50f7e4cd679c667ca92397135936d11abad31f1
juanflorezro <juan10florez10@gmail.com>
2024-08-16 16:25:34 -0500

    agrar ruta para consulatr el servudor

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,6 +4,9 @@ const loginControllers = require('../controllers/loginControllers')
 router.get('/', loginControllers.listar)
 router.get('/buscar', loginControllers.listarB)
 router.post('/crear', loginControllers.agregar)
+router.get('/servidor', (req, res) => {
+    res.send('Servidor En EJECUCION')
+})
 router.post('/autenticacion', loginControllers.autenticacion)
 router.post('/validacion', loginControllers.validacion)
 
